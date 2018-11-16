ab2b48fc3e6428af9ab0a5a369f06a293710b636
hilla <hilladalins@gmail.com>
2018-11-16 20:05:15 +0200

    add levels

--- js/myscripts.js
+++ js/myscripts.js
@@ -1,31 +1,84 @@
 var lulu = {}
 
+lulu.world = ["spain", "netherlands", "jamaica", "argentina", "romania", "australia", "kenya", "italy","norway", "canada"];
+lulu.rap = ["eminem", "jay-z", "kendrick-lamar", "nas", "snoop-dog", "two-pac", "cardi-b", "notorious-big", "nicki-minaj", "ice-cube"];
+lulu.flowers = ["calanit", "irus", "narkis", "rakefet", "sitvanit", "tzivoni", "dvoranit", "shoshan", "turmus", "iris"];
+lulu.easy = 12;
+lulu.medium = 16;
+lulu.hard = 20;
+
 lulu.start = function () {
+    lulu.board = $(".board-wrap");
+    lulu.modal = $("#modal-container");
+    lulu.connectButtonsEvent();
+}
+
+lulu.connectButtonsEvent = function () {
+    $(".new-game-button").on("click", lulu.newGame);
+};
+
+lulu.newGame = function () {
+    lulu.board.empty();
+    lulu.modal.css("display", "none");
+    $("audio").remove();
+    $("body").removeClass();
     lulu.setDefault();
+    lulu.setBoard();
+    lulu.setSubject();
+    lulu.connectClickToCards();
+    lulu.startPlayingAudio();
+};
+
+lulu.setDefault = function () {
+    lulu.level = $("#level option:selected").val()
+    lulu.numberOfCards = lulu[lulu.level];
+    lulu.cardsFlipped = [];
+    lulu.trialsCounter = 0;
+};
+
+lulu.setBoard = function () {
+    lulu.board.removeClass();
+    lulu.board.addClass("board-wrap");
+    lulu.board.addClass(lulu.level);
+    var trialsCounter = $("<span/>");
+    trialsCounter.addClass("trials-counter");
+    trialsCounter.text(lulu.trialsCounter)
+    var trials = $("<div/>");
+    trials.addClass("trials");
+    trials.text("Number of moves: ")
+    trials.append(trialsCounter);
+    var cardWrap = $("<div/>");
+    cardWrap.addClass("cards-wrap");
+    lulu.board.append(trials);
+    lulu.board.append(cardWrap);
+    for (var i = 0; i < parseInt(lulu.numberOfCards); i++) {
+        var cardContainer = $("<div/>");
+        cardContainer.addClass("card-container");
+        cardContainer.addClass(`${lulu.level}-card`);
+        var card = $("<div/>");
+        card.addClass("card");
+        var cardImgBack = $("<div/>");
+        cardImgBack.addClass("card-img");
+        cardImgBack.addClass("back");
+        cardWrap.append(cardContainer);
+        cardContainer.append(card);
+        card.append(cardImgBack);
+    }
+}
+
+lulu.setSubject = function () {
+    lulu.subject = $("#subject option:selected").val();
     lulu.setBackground();
     lulu.setBackCards();
     lulu.createArrOfImg();
     lulu.createArrayOfRandomIndexes();
     lulu.enterCardsToTheBoard();
-    lulu.connectButtonsEvent();
-    lulu.connectClickToCards();
+    $(".board-wrap").css("display", "block");
     lulu.startPlayingAudio();
 }
 
-lulu.world = ["spain", "netherlands", "jamaica", "argentina", "romania", "australia"];
-lulu.rap = ["eminem", "jay-z", "kendrick-lamar", "nas", "snoop-dog","two-pac"];
-lulu.flowers = ["calanit", "irus", "narkis", "rakefet", "sitvanit", "tzivoni"];
 
-lulu.setDefault = function () {
-    lulu.subject = "flowers"
-    lulu.cardsFlipped = [];
-    lulu.trialsCounter = 0;
-    $(".trials-counter").text(lulu.trialsCounter);
-};
 
-lulu.newGame = function () {
-    location.reload();
-};
 
 lulu.setBackground = function () {
     $("body").addClass(lulu.subject);
@@ -33,7 +86,7 @@ lulu.setBackground = function () {
 
 lulu.setBackCards = function () {
     $(".back").css({
-        "background" :`url("./img/${lulu.subject}/${lulu.subject}-back.jpg")`,
+        "background": `url("./img/${lulu.subject}/${lulu.subject}-back.jpg")`,
         "background-size": "cover"
     })
 }
@@ -80,22 +133,18 @@ lulu.enterCardsToTheBoard = function () {
     })
 };
 
-lulu.connectButtonsEvent = function () {
-    $(".new-game-button").on("click", lulu.newGame);
-};
-
 lulu.connectClickToCards = function () {
     $(".non-matched").on("click", lulu.play);
 };
 
-lulu.startPlayingAudio = function (){
+lulu.startPlayingAudio = function () {
     var audio = document.createElement("audio");
     audio.id = "game-audio";
-    audio.src = "./audio/world.mp3"
+    audio.src = `./audio/${lulu.subject}.mp3`
     audio.type = "audio/mpeg";
     audio.preload = "auto";
     audio.loop = "true";
-    audio.volume = 1;
+    audio.volume = 0.3;
     audio.autoplay = "true";
     document.getElementsByTagName("body")[0].appendChild(audio);
     audio.load();
@@ -133,16 +182,15 @@ lulu.checkIfCorrect = function () {
         lulu.cardsFlipped[0].off("click", lulu.play);
         lulu.cardsFlipped[1].off("click", lulu.play);
         if ($(".non-matched").length === 0) {
-            setTimeout(function(){
+            setTimeout(function () {
                 $("#modal-container").css("display", "block");
-            },600);            
+            }, 600);
         }
         lulu.cardsFlipped = [];
         lulu.connectClickToCards();
     }
 
 };
-
 
 $(document).ready(function () {
     lulu.start();
